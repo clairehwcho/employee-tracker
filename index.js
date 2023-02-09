24418428a01e2bb33ad87ce9e43ad120e395d27b
Claire <claire.hw.cho@gmail.com>
2023-02-09 11:54:50 -0800

    complete view and add features

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const Department = require('./lib/Department');
 const Role = require('./lib/Role');
 const Employee = require('./lib/Employee');
 
+const department = new Department();
+const role = new Role();
+const employee = new Employee();
+
 // A list of questions for initial prompt
 const initialQuestions = [
     {
@@ -12,42 +16,120 @@ const initialQuestions = [
         message: 'What would you like to do?',
         choices: [
             'View all departments',
+            'Add a department',
             'View all roles',
+            'Add a role',
             'View all employees',
-            // 'Add a department',
-            // 'Add a role',
-            // 'Add an employee',
-            // 'Update an employee role'
+            'Add an employee',
+            'Update an employee role',
             'Quit'
         ]
     }
 ];
 
+const questionToAddDepartment = [
+    {
+        name: 'name',
+        type: 'input',
+        message: 'What is the name of the department?'
+    }
+];
+
+const questionsToAddRole = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of the role?',
+    },
+    {
+        name: 'salary',
+        type: 'number',
+        message: 'What is the salary of the role?'
+    },
+    {
+        name: 'departmentName',
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        choices: function () {
+            return department.getDepartmentNames();
+        }
+    }
+];
+
+const questionsToAddEmployee = [
+    {
+        name: 'firstName',
+        type: 'input',
+        message: 'What is the employee\'s first name?'
+    },
+    {
+        name: 'lastName',
+        type: 'input',
+        message: 'What is the employee\'s last name?'
+    },
+    {
+        name: 'roleTitle',
+        type: 'list',
+        message: 'What is the employee\'s role?',
+        choices: function () {
+            return role.getRoleTitles();
+        }
+    },
+    {
+        name: 'managerName',
+        type: 'list',
+        message: 'Who is the employee\'s manager?',
+        choices: function () {
+            return employee.getEmployeeNames();
+        }
+    },
+];
+
 const init = function () {
     inquirer
         .prompt(initialQuestions)
-        .then((answers) => {
+        .then(async (answers) => {
             switch (answers.userChoice) {
                 case 'View all departments':
-                    const department = new Department();
                     department.showAllDepartments()
                         .then(() => {
                             init();
                         });
                     break;
+                case 'Add a department':
+                    const newDepartmentObj = await inquirer.prompt(questionToAddDepartment);
+                    department.addDepartment(newDepartmentObj.name)
+                        .then(() => {
+                            init();
+                        });
+                    break;
                 case 'View all roles':
-                    const role = new Role();
                     role.showAllRoles()
                         .then(() => {
                             init();
                         });
                     break;
+                case 'Add a role':
+                    const newRoleObj = await inquirer.prompt(questionsToAddRole);
+                    const { title, salary, departmentName } = newRoleObj;
+                    role.addRole(title, salary, departmentName)
+                        .then(() => {
+                            init();
+                        });
+                    break;
                 case 'View all employees':
-                    const employee = new Employee();
                     employee.showAllEmployees()
                         .then(() => {
                             init();
                         });
+                    break;
+                case 'Add an employee':
+                    const newEmployeeObj = await inquirer.prompt(questionsToAddEmployee);
+                    const { firstName, lastName, roleTitle, managerName } = newEmployeeObj;
+                    employee.addEmployee(firstName, lastName, roleTitle, managerName)
+                        .then(() => {
+                            init();
+                        })
                     break;
                 case 'Quit':
                     connection.end();
